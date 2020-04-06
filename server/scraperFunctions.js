da3bb8547eeb02ab52c1f0bd06f7ac5b1a669816
robert-cummins <robert.k.cummins@gmail.com>
2020-04-06 16:12:19 +1200

    object with product and price returned from 1 page new world

--- server/scraperFunctions.js
+++ server/scraperFunctions.js
@@ -1,23 +1,45 @@
 const puppeteer = require('puppeteer');
 
-async function scrapeSite(url){
+async function scrapeSite(url) {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-
-    await page.goto(url)
-
-    let data = await page.evaluate(() => {
-        let titles = [... document.querySelectorAll('div[class="fs-product-card__description"] > h3')]
-        return titles.map((title) => title.textContent.trim())
+    await page.goto(url, {waitUntil: 'networkidle2'})
+    await page.evaluate(()=> {
+        window.scrollBy(0, window.innerHeight)
     })
-    
+    await page.waitForSelector('.fs-product-card')
 
-   console.log(data)
-    
+    const test = await page.$$(".fs-product-card")
+
+    const testHandle = await Promise.all(test.map(handle => {
+        return handle.getProperty('innerText')
+    }))
+
+    const jsTestHandle = await Promise.all(testHandle.map(handle => {
+        return handle.jsonValue()
+    }))
+
+    const trimed = jsTestHandle.map(el => {
+        return el.split(/\r?\n/)
+    })
+
+    const obj = trimed.map((el) => {
+        productObject = {name: el[0], price: ''}
+        if((el[el.length-1] == 'ea' || el[el.length-1] == 'kg')){
+            productObject.price = `${el[el.length-3]}.${el[el.length-2]}`
+        } else if ((el[el.length-2] == 'ea' || el[el.length-2] == 'kg')){
+            productObject.price = `${el[el.length-4]}.${el[el.length-3]}`
+        }
+
+        return productObject  
+    })
+    console.log(obj)
+
 }
 
-scrapeSite('https://www.ishopnewworld.co.nz/category/fresh-foods-and-bakery')
+scrapeSite('https://www.ishopnewworld.co.nz/category/fresh-foods-and-bakery/fruit--vegetables')
 
 module.exports = {
     scrapeSite
 }
+
