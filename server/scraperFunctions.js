ab70a5051326f4431b802e0d8bb95633b55c1e3d
robert-cummins <robert.k.cummins@gmail.com>
2020-04-06 16:26:55 +1200

    Clean up scraper code

--- server/scraperFunctions.js
+++ server/scraperFunctions.js
@@ -4,40 +4,38 @@ async function scrapeSite(url) {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.goto(url, {waitUntil: 'networkidle2'})
-    await page.evaluate(()=> {
-        window.scrollBy(0, window.innerHeight)
-    })
-    await page.waitForSelector('.fs-product-card')
+    
+    const elements = await page.$$(".fs-product-card")
 
-    const test = await page.$$(".fs-product-card")
-
-    const testHandle = await Promise.all(test.map(handle => {
+    const elementHandles = await Promise.all(elements.map(handle => {
         return handle.getProperty('innerText')
     }))
 
-    const jsTestHandle = await Promise.all(testHandle.map(handle => {
+    const elementText = await Promise.all(elementHandles.map(handle => {
         return handle.jsonValue()
     }))
 
-    const trimed = jsTestHandle.map(el => {
+    const elementTextTrimed = elementText.map(el => {
         return el.split(/\r?\n/)
     })
 
-    const obj = trimed.map((el) => {
-        productObject = {name: el[0], price: ''}
+    const dataObject = elementTextTrimed.map((el) => {
+        productObject = {name: el[0], price: '', type: ''}
         if((el[el.length-1] == 'ea' || el[el.length-1] == 'kg')){
+            productObject.type = el[el.length-1]
             productObject.price = `${el[el.length-3]}.${el[el.length-2]}`
         } else if ((el[el.length-2] == 'ea' || el[el.length-2] == 'kg')){
+            productObject.type = el[el.length-2]
             productObject.price = `${el[el.length-4]}.${el[el.length-3]}`
         }
 
         return productObject  
     })
-    console.log(obj)
+    console.log(dataObject)
 
 }
 
-scrapeSite('https://www.ishopnewworld.co.nz/category/fresh-foods-and-bakery/fruit--vegetables')
+scrapeSite('https://www.ishopnewworld.co.nz/category/fresh-foods-and-bakery')
 
 module.exports = {
     scrapeSite
