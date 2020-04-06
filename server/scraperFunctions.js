d3f33c3fad1d07dfd581f1edc74fab97ebf2c418
robert-cummins <robert.k.cummins@gmail.com>
2020-04-07 10:57:15 +1200

    break large scraper function into smaller functions

--- server/scraperFunctions.js
+++ server/scraperFunctions.js
@@ -3,9 +3,21 @@ const puppeteer = require('puppeteer');
 async function scrapeSite(url) {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto(url, {waitUntil: 'networkidle2'})
-    
-    const elements = await page.$$(".fs-product-card")
+    await page.goto(url, { waitUntil: 'networkidle2' })
+
+    const elementText = await scrapeNewworldTextData(page, ".fs-product-card")
+    const newWorldData = await getNewworldDataObject(elementText)
+
+
+    console.log(newWorldData)
+
+}
+
+
+
+
+async function scrapeNewworldTextData(page, element) {
+    const elements = await page.$$(element)
 
     const elementHandles = await Promise.all(elements.map(handle => {
         return handle.getProperty('innerText')
@@ -15,24 +27,28 @@ async function scrapeSite(url) {
         return handle.jsonValue()
     }))
 
-    const elementTextTrimed = elementText.map(el => {
+    return elementText.map(el => {
         return el.split(/\r?\n/)
     })
+}
 
-    const dataObject = elementTextTrimed.map((el) => {
-        productObject = {name: el[0], price: '', type: ''}
-        if((el[el.length-1] == 'ea' || el[el.length-1] == 'kg')){
-            productObject.type = el[el.length-1]
-            productObject.price = `${el[el.length-3]}.${el[el.length-2]}`
-        } else if ((el[el.length-2] == 'ea' || el[el.length-2] == 'kg')){
-            productObject.type = el[el.length-2]
-            productObject.price = `${el[el.length-4]}.${el[el.length-3]}`
+
+
+
+function getNewworldDataObject(trimedArr) {
+    let dataArray = []
+    trimedArr.map((el) => {
+        productObject = { name: el[0], price: '', type: '' }
+        if ((el[el.length - 1] == 'ea' || el[el.length - 1] == 'kg')) {
+            productObject.type = el[el.length - 1]
+            productObject.price = `${el[el.length - 3]}.${el[el.length - 2]}`
+        } else if ((el[el.length - 2] == 'ea' || el[el.length - 2] == 'kg')) {
+            productObject.type = el[el.length - 2]
+            productObject.price = `${el[el.length - 4]}.${el[el.length - 3]}`
         }
-
-        return productObject  
+        return dataArray.push(productObject)
     })
-    console.log(dataObject)
-
+    return dataArray
 }
 
 scrapeSite('https://www.ishopnewworld.co.nz/category/pantry')
