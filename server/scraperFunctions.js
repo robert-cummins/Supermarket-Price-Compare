3ab29201f16f211664992ec83c6a7d5ac47b8572
robert-cummins <robert.k.cummins@gmail.com>
2020-04-08 12:35:36 +1200

    functions return pak and save data from 1 page

--- server/scraperFunctions.js
+++ server/scraperFunctions.js
@@ -12,24 +12,55 @@ async function scrapeSites() {
             request.continue();
     });
 
-    let newWorldDataArray = []
-    let countdownDataArray = []
-    for (let i = 1; i <= 4; i++) {
-        if (i <= 3) {
-            await page.goto(`https://shop.countdown.co.nz/shop/browse/fruit-vegetables?ps=120&page=${i}`, { waitUntil: 'networkidle2' })
-            const countdownElementTextArr = await scrapeNewworldTextData(page, ".product-entry")
-            let countdownData = getCountdownDataObject(countdownElementTextArr)
-            countdownDataArray.push(countdownData)
-        }
-        await page.goto(`https://www.ishopnewworld.co.nz/category/fresh-foods-and-bakery/fruit--vegetables?ps=50&pg=${i}`, { waitUntil: 'networkidle2' })
-        const newWorldElementTextArr = await scrapeNewworldTextData(page, ".fs-product-card")
-        const newWorldData = await getNewworldDataObject(newWorldElementTextArr)
-        newWorldDataArray.push(newWorldData)
+    function seperatePakSavePriceFromType(arr, arrIndex){
+        let numLettterSplitArr = arr[arr.length-arrIndex].match(/[a-zA-Z]+|[0-9]+/g)
+        productObject.type = numLettterSplitArr[1]
+        if(numLettterSplitArr[0].length == 3){
+                productObject.price = `${numLettterSplitArr[0].slice(0, 1)}.${numLettterSplitArr[0].slice(1, 3)}` 
+            } else{
+                productObject.price = `${numLettterSplitArr[0].slice(0, 2)}.${numLettterSplitArr[0].slice(2, 4)}` 
+            }
     }
 
-    console.log(newWorldDataArray)
-    console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
-    console.log(countdownDataArray)
+    await page.goto(`https://www.paknsaveonline.co.nz/category/fresh-foods-and-bakery/fruit--vegetables?ps=50&pg=1`, { waitUntil: 'networkidle2' })
+    // console.log(page)
+    const pakSaveElementTextArr = await scrapeNewworldTextData(page, ".fs-product-card")
+    let obj = pakSaveElementTextArr.map(arr => {
+        productObject = { name: arr[0], price: '', type: '' }
+        if(arr[arr.length-4].charAt(0) == '$'){
+            seperatePakSavePriceFromType(arr, 5)
+            
+        } else {
+            seperatePakSavePriceFromType(arr, 4)
+
+        }
+        return productObject
+        
+        
+        
+        
+        
+    })
+    console.log(obj)
+
+    // let newWorldDataArray = []
+    // let countdownDataArray = []
+    // for (let i = 1; i <= 4; i++) {
+    //     if (i <= 3) {
+    //         await page.goto(`https://shop.countdown.co.nz/shop/browse/fruit-vegetables?ps=120&page=${i}`, { waitUntil: 'networkidle2' })
+    //         const countdownElementTextArr = await scrapeNewworldTextData(page, ".product-entry")
+    //         let countdownData = getCountdownDataObject(countdownElementTextArr)
+    //         countdownDataArray.push(countdownData)
+    //     }
+    //     await page.goto(`https://www.ishopnewworld.co.nz/category/fresh-foods-and-bakery/fruit--vegetables?ps=50&pg=${i}`, { waitUntil: 'networkidle2' })
+    //     const newWorldElementTextArr = await scrapeNewworldTextData(page, ".fs-product-card")
+    //     const newWorldData = await getNewworldDataObject(newWorldElementTextArr)
+    //     newWorldDataArray.push(newWorldData)
+    // }
+
+    // console.log(newWorldDataArray)
+    // console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+    // console.log(countdownDataArray)
 }
 
 
