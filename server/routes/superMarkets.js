96b5be5ac94203234c57c760d0fd89c2bc831f92
robert-cummins <robert.k.cummins@gmail.com>
2020-04-14 11:57:03 +1200

    Add all supermarket routes

---
+++ server/routes/superMarkets.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const router = express.Router()
+const scraper = require('../scraperFunctions')
+const Supermarket = require('../models/productDetails.model')
+
+
+router.get('/pakandsave', (req, res) => {
+  Supermarket.pakAndSave.find((err, supermarketData) => {
+    if(err){
+      console.log("There is an error: " + err)
+    } else {
+      res.json(supermarketData)
+    }
+  })
+})
+
+
+router.get('/countdown', (req, res) => {
+  Supermarket.countdown.find((err, supermarketData) => {
+    if(err){
+      console.log("There is an error: " + err)
+    } else {
+      res.json(supermarketData)
+    }
+  })
+})
+
+router.get('/newworld', (req, res) => {
+  Supermarket.newWorld.find((err, supermarketData) => {
+    if(err){
+      console.log("There is an error: " + err)
+    } else {
+      res.json(supermarketData)
+    }
+  })
+})
+
+
+    
+module.exports = router
