3b16aa57ebcf12014634a406d798f45b9a647e58
robert-cummins <robert.k.cummins@gmail.com>
2020-04-21 17:11:21 +1200

    selected item gos to redux state

--- client/components/SearchResults.jsx
+++ client/components/SearchResults.jsx
@@ -1,46 +1,48 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { getSearchedNewWorldItems } from '../actions'
+import { getSelectedItem} from '../actions'
 
-const SearchResults = (props) => {
-
-    const handleClick = (e) => {
-        console.log(e.target.getAttribute('name'))
+class SearchResults extends React.Component  {
+    constructor(props){
+        super(props)
+        
     }
-
-    if (props[props.supermarket].length != 0) {
-        return (
-            <table className="ui selectable celled table">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>Sold by</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {props[props.supermarket].map((item, i) => {
-                        if (item.type == 'kg') { item.type = '/ kg' }
-                        else if(item.type == 'ea' && item.weight != 'N/A'){item.type = 'Each @ ' + item.weight}
-                        else { item.type = "Each"}
-                        return (
-                            <tr name={item.name} onClick={handleClick} key={i}>
-                                <td name={item.name}>{item.name}</td>
-                                <td name={item.name}>{item.price}</td>
-                                
-                                <td name={item.name}>{item.type}</td>
-                                <td><button class="ui primary basic tiny button">Add Item</button></td>
-                            </tr>
-                        )
-                    })}
-
-                </tbody>
-            </table>
-        )
-    } else {
-        return <div></div>
+    
+    render(){
+        if (this.props[this.props.supermarket].length != 0) {
+            return (
+                <table className="ui selectable celled table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Sold by</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props[this.props.supermarket].map((item, i) => {
+                            if (item.type == 'kg') { item.type = '/ kg' }
+                            else if(item.type == 'ea' && item.weight != 'N/A'){item.type = 'Each @ ' + item.weight}
+                            else { item.type = "Each"}
+                            return (
+                                <tr key={i}>
+                                    <td>{item.name}</td>
+                                    <td>{item.price}</td>
+                                    <td>{item.type}</td>
+                                    <td><button onClick={() => this.props.dispatch(getSelectedItem(item))}  name={item.name} className="ui primary basic tiny button">Add Item</button></td>
+                                </tr>
+                            )
+                        })}
+    
+                    </tbody>
+                </table>
+            )
+        } else {
+            return <div></div>
+        }
     }
+    
 
 }
 
