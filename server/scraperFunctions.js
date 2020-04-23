f1aa551fa734604a4bd232742cc4c89f953952ba
robert-cummins <robert.k.cummins@gmail.com>
2020-04-23 15:04:07 +1200

    All pak save items scraped

--- server/scraperFunctions.js
+++ server/scraperFunctions.js
@@ -5,13 +5,13 @@ const NewWorldProduct = mongoose.model('New World')
 const CountdownProduct = mongoose.model('Countdown')
 const PakAndSaveProduct = mongoose.model('Pak and Save')
 
-async function scrapeNewWorld(url, pageNum, context, page, browser) {
-    
+async function scrapeNewWorldPakSave(url, pageNum, context, page, marketModel, marketName) {
+
     await context.overridePermissions(url + pageNum, ['geolocation'])
     await page.goto(url + pageNum, { waitUntil: 'networkidle2' })
     const newWorldElementTextArr = await scrapeSuperMarketTextData(page, ".fs-product-card")
-    const newWorldData = await getNewworldOrPakSaveDataObject(newWorldElementTextArr, 'NewWorld')
-    insertData(newWorldData, NewWorldProduct)
+    const newWorldData = await getNewworldOrPakSaveDataObject(newWorldElementTextArr, marketName)
+    insertData(newWorldData, marketModel)
 }
 
 async function scrapeSites() {
@@ -22,47 +22,64 @@ async function scrapeSites() {
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
     const context = browser.defaultBrowserContext();
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
 
     for (let i = 1; i <= 20; i++) {
-
-    await scrapeNewWorld("https://www.ishopnewworld.co.nz/category/fresh-foods-and-bakery?ps=50&pg=", i, context, page, browser)
-    await scrapeNewWorld("https://www.ishopnewworld.co.nz/category/chilled-frozen-and-desserts/cheese?ps=50&pg=", i, context, page, browser)
-    await scrapeNewWorld("https://www.ishopnewworld.co.nz/category/chilled-frozen-and-desserts/desserts?ps=50&pg=", i, context, page, browser)
-    await scrapeNewWorld("https://www.ishopnewworld.co.nz/category/chilled-frozen-and-desserts/frozen-foods?ps=50&pg=", i, context, page, browser)
-    await scrapeNewWorld("https://www.ishopnewworld.co.nz/category/pantry?ps=50&pg=", i, context, page, browser)
-    await scrapeNewWorld("https://www.ishopnewworld.co.nz/category/drinks/cold-drinks?ps=50&pg=", i, context, page, browser)
-    await scrapeNewWorld("https://www.ishopnewworld.co.nz/category/beer-cider-and-wine?ps=50&pg=", i, context, page, browser)
-    await scrapeNewWorld("https://www.ishopnewworld.co.nz/category/personal-care?ps=50&pg=", i, context, page, browser)
-    await scrapeNewWorld("https://www.ishopnewworld.co.nz/category/baby-toddler-and-kids?ps=50&pg=", i, context, page, browser)
-    
-   
-
-    
-
-
-    // await context.overridePermissions(`https://www.paknsaveonline.co.nz/category/fresh-foods-and-bakery/fruit--vegetables?ps=50&pg=${i}`, ['geolocation'])
-    // await page.goto(`https://www.paknsaveonline.co.nz/category/fresh-foods-and-bakery/fruit--vegetables?ps=50&pg=${i}`, { waitUntil: 'networkidle2' })
-    // const pakSaveElementTextArr = await scrapeSuperMarketTextData(page, ".fs-product-card")
-    // const pakData = getNewworldOrPakSaveDataObject(pakSaveElementTextArr, 'PakSave')
-    // insertData(pakData, PakAndSaveProduct)
-
-    // await page.goto(`https://shop.countdown.co.nz/shop/browse/fruit-vegetables?page=${i}`, { waitUntil: 'networkidle2' })
-    // const countdownElementTextArr = await scrapeSuperMarketTextData(page, ".product-entry")
-    // // console.log(countdownElementTextArr)
-    // const countdownData = getCountdownDataObject(countdownElementTextArr)
-    // insertData(countdownData, CountdownProduct)
+        if (i <= 3) { scrapeNewWorldPakSave("https://www.paknsaveonline.co.nz/category/chilled-frozen-and-desserts/desserts?ps=50&pg=", i, context, page, PakAndSaveProduct, 'PakSave') }
+        if (i <= 5) { await scrapeNewWorldPakSave("https://www.ishopnewworld.co.nz/category/chilled-frozen-and-desserts/desserts?ps=50&pg=", i, context, page, NewWorldProduct, 'NewWorld') }
+        if (i <= 6) {
+            await scrapeNewWorldPakSave("https://www.ishopnewworld.co.nz/category/chilled-frozen-and-desserts/cheese?ps=50&pg=", i, context, page, NewWorldProduct, 'NewWorld')
+            await scrapeNewWorldPakSave("https://www.paknsaveonline.co.nz/category/chilled-frozen-and-desserts/cheese?ps=50&pg=", i, context, page, PakAndSaveProduct, 'PakSave')
+            await scrapeNewWorldPakSave("https://www.paknsaveonline.co.nz/category/chilled-frozen-and-desserts/frozen-foods?ps=50&pg=", i, context, page, PakAndSaveProduct, 'PakSave')
+            await scrapeNewWorldPakSave("https://www.paknsaveonline.co.nz/category/baby-toddler-and-kids?ps=50&pg=", i, context, page, PakAndSaveProduct, 'PakSave')
+        }
+        if (i <= 7) { await scrapeNewWorldPakSave("https://www.ishopnewworld.co.nz/category/baby-toddler-and-kids?ps=50&pg=", i, context, page, NewWorldProduct, 'NewWorld') }
+        if (i <= 9) {
+            await scrapeNewWorldPakSave("https://www.ishopnewworld.co.nz/category/chilled-frozen-and-desserts/frozen-foods?ps=50&pg=", i, context, page, NewWorldProduct, 'NewWorld')
+            await scrapeNewWorldPakSave("https://www.paknsaveonline.co.nz/category/drinks/cold-drinks?ps=50&pg=", i, context, page, PakAndSaveProduct, 'PakSave')
+        }
+        if (i <= 14) {
+            await scrapeNewWorldPakSave("https://www.ishopnewworld.co.nz/category/beer-cider-and-wine?ps=50&pg=", i, context, page, NewWorldProduct, 'NewWorld')
+            await scrapeNewWorldPakSave("https://www.ishopnewworld.co.nz/category/drinks/cold-drinks?ps=50&pg=", i, context, page, NewWorldProduct, 'NewWorld')
+        }
+        if (i <= 15) { await scrapeNewWorldPakSave("https://www.paknsaveonline.co.nz/category/beer-cider-and-wine?ps=50&pg=", i, context, page, PakAndSaveProduct, 'PakSave') }
+        await scrapeNewWorldPakSave("https://www.ishopnewworld.co.nz/category/fresh-foods-and-bakery?ps=50&pg=", i, context, page, NewWorldProduct, 'NewWorld')
+        await scrapeNewWorldPakSave("https://www.ishopnewworld.co.nz/category/pantry?ps=50&pg=", i, context, page, NewWorldProduct, 'NewWorld')
+        await scrapeNewWorldPakSave("https://www.ishopnewworld.co.nz/category/personal-care?ps=50&pg=", i, context, page, NewWorldProduct, 'NewWorld')
+        await scrapeNewWorldPakSave("https://www.paknsaveonline.co.nz/category/personal-care?ps=50&pg=", i, context, page, PakAndSaveProduct, 'PakSave')
+        await scrapeNewWorldPakSave("https://www.paknsaveonline.co.nz/category/fresh-foods-and-bakery?ps=50&pg=", i, context, page, PakAndSaveProduct, 'PakSave')
+        await scrapeNewWorldPakSave("https://www.paknsaveonline.co.nz/category/pantry?ps=50&pg=", i, context, page, PakAndSaveProduct, 'PakSave')
 
 
-}
-await browser.close();
+
+
+
+
+
+        // await context.overridePermissions(`https://www.paknsaveonline.co.nz/category/fresh-foods-and-bakery/fruit--vegetables?ps=50&pg=${i}`, ['geolocation'])
+        // await page.goto(`https://www.paknsaveonline.co.nz/category/fresh-foods-and-bakery/fruit--vegetables?ps=50&pg=${i}`, { waitUntil: 'networkidle2' })
+        // const pakSaveElementTextArr = await scrapeSuperMarketTextData(page, ".fs-product-card")
+        // const pakData = getNewworldOrPakSaveDataObject(pakSaveElementTextArr, 'PakSave')
+        // insertData(pakData, PakAndSaveProduct)
+
+        // await page.goto(`https://shop.countdown.co.nz/shop/browse/fruit-vegetables?page=${i}`, { waitUntil: 'networkidle2' })
+        // const countdownElementTextArr = await scrapeSuperMarketTextData(page, ".product-entry")
+        // // console.log(countdownElementTextArr)
+        // const countdownData = getCountdownDataObject(countdownElementTextArr)
+        // insertData(countdownData, CountdownProduct)
+
+
+    }
+    await browser.close();
 }
 
 
@@ -73,7 +90,7 @@ function insertData(arr, superMarket) {
         supermarketProduct.price = el.price
         supermarketProduct.type = el.type
         supermarketProduct.weight = el.weight,
-            supermarketProduct.supermarket = el.supermarket
+        supermarketProduct.supermarket = el.supermarket
         supermarketProduct.save((err, doc) => {
             if (!err) {
                 console.log("success")
