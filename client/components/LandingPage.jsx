9337fc45100fa7f11bd76f540b28f9d20129d524
robert-cummins <robert.k.cummins@gmail.com>
2020-05-12 13:09:18 +1200

    Add stlyong to headings and buttons

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -47,7 +47,7 @@ class LandingPage extends React.Component {
         return (
             <>
                 <div className="ui container">
-                    <h1 className="ui center aligned header">Super Market Price Compare</h1>
+                    <h1 className="main-text">Super Market Price Compare</h1>
                     <SearchBar />
                 </div>
 
@@ -62,25 +62,25 @@ class LandingPage extends React.Component {
 
                 <div id="context1">
                     <div className="ui secondary menu tabs">
-                        <a onClick={this.handleClick} name="search" className={this.state.activeTab == 'search' ? 'item active' : 'item'} data-tab="first">Search</a>
-                        <a onClick={this.handleClick} name="shopping" className={this.state.activeTab == 'shopping' ? 'item active' : 'item'} data-tab="second">Shopping Basket</a>
+                        <a onClick={this.handleClick} name="search" className={this.state.activeTab == 'search' ? 'item active tab' : 'item tab-hover'} data-tab="first">Search Results</a>
+                        <a onClick={this.handleClick} name="shopping" className={this.state.activeTab == 'shopping' ? 'item active tab' : 'item tab-hover'} data-tab="second">Shopping Basket</a>
                     </div>
                 </div>
                 {this.state.activeTab == 'search' &&
                     <div className="table-container">
                         <div className="ui three column doubling stackable grid">
                             <div className="column">
-                                <h2 className="market-headers">New World</h2>
+                                <h2 className="market-headers newworld">NEW WORLD</h2>
                                 <SearchResults supermarket={'searchedNewWorldItems'} />
                             </div>
 
                             <div className="column">
-                                <h2 className="market-headers">Countdown</h2>
+                                <h2 className="market-headers countdown">Countdown</h2>
                                 <SearchResults supermarket={'searchedCountdownItems'} />
                             </div>
 
                             <div className="column">
-                                <h2 className="market-headers">Pak and Save</h2>
+                                <h2 className="market-headers paknsave">PAK'nSAVE</h2>
                                 <SearchResults supermarket={'searchedPakSaveItems'} />
                             </div>
                         </div>
@@ -91,16 +91,16 @@ class LandingPage extends React.Component {
                     <div className="table-container">
                         <div className="ui three column doubling stackable grid">
                             <div className="column">
-                                <h2 className="market-headers">New World</h2>
+                                <h2 className="market-headers newworld">NEW WORLD</h2>
                                 <ShoppingBasket supermarket={'NewWorld'} />
                             </div>
 
                             <div className="column">
-                                <h2 className="market-headers">Countdown</h2>
+                                <h2 className="market-headers countdown">Countdown</h2>
                                 <ShoppingBasket supermarket={'Countdown'} />
                             </div>
                             <div className="column">
-                                <h2 className="market-headers">Pak and Save</h2>
+                                <h2 className="market-headers paknsave">PAK'nSAVE</h2>
                                 <ShoppingBasket supermarket={'PakSave'} />
                             </div>
                         </div>
