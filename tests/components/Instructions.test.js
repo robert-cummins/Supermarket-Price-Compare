716f9d85b21cc5bdc0f62e821388fe50f282ffd2
robert-cummins <robert.k.cummins@gmail.com>
2020-05-25 16:50:08 +1200

    Add Intructions component tests

---
+++ tests/components/Instructions.test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import Instructions from '../../client/components/Instructions'
+
+const setUp = (props={}) => {
+    const component = shallow(<Instructions {...props} />)
+    return component
+}
+
+describe('Instructions Component', () => {
+
+    let component
+
+    beforeEach(() => {
+        component = setUp()
+    })
+
+    test('Instructions renders without error', () => {
+        const wrapper = component.find('p')
+        expect(wrapper.length).toBe(1)
+    })
+
+    test('Instructions should have 4 bullet point instructions', () => {
+        const wrapper = component.find('li')
+        expect(wrapper.length).toEqual(4)
+    })
+})
