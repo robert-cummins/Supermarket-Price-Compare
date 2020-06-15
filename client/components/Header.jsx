9f963ea004bfd068eb4614590074bc9f4111048d
robert-cummins <robert.k.cummins@gmail.com>
2020-06-15 16:11:31 +1200

    Make changes to instructions

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <h1 className="main-text">Supermarket Price Compare</h1>
+        <h1 className="main-text">SUPERMARKET PRICE COMPARE</h1>
     )
 }
 
--- client/components/Instructions.jsx
+++ client/components/Instructions.jsx
@@ -4,17 +4,17 @@ const Instructions = () => {
     return (
         <div className="ui container">
             <p className="intro">
-                Welcome to Supermarket Price Compare. This app allows you to see exactly how much your shopping list or an individual item will cost at New Zealands biggest supermarket chains. 
+                Welcome to Supermarket Price Compare. This app allows you to see exactly how much an individual item or whole shopping list will cost at New Zealand's biggest supermarket chains. 
             </p>
             <ul>
                 <li className="instructions">
-                    Search for items on your shopping list, e.g., "nescafe coffee" using the search bar and hit enter or press the search button.
+                    Search for items on your shopping list, e.g., "nescafe coffee" using the search bar. Results will be shown for all 3 supermarkets under the "Search Results" tab.
                 </li>
                 <li className="instructions">
-                    Results will be shown for all 3 supermarkets under the "Search Results" tab which will be automatically selected after a search.
+                    If you would like to see the total cost of multiple items at a supermarket, use the "add item" button to keep track.
                 </li>
                 <li className="instructions">
-                    Add your desired item for each supermarket.
+                    Add the same items at each supermarket if you would like to compare total prices between them.
                 </li>
                 <li className="instructions">
                     Large search results can be narrowed using the category checkboxes. Being as specific as possible with your search, e.g., searching the brand name as well as the product name or adding the word "produce" when searching for fresh vegatables, e.g., "produce carrots" will also help narrow search results.
