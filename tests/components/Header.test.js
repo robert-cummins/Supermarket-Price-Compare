1518a6a5bdfe2390f4da11022f93eaddb9543f7c
robert-cummins <robert.k.cummins@gmail.com>
2020-08-02 11:54:13 +1200

    tests refactored to reflect changes

--- tests/components/Header.test.js
+++ tests/components/Header.test.js
@@ -20,7 +20,7 @@ describe('Header Component', () => {
     })
 
     test('App title is rendered', () => {
-        expect(wrapper.text()).toEqual('Supermarket Saver')
+        expect(wrapper.text()).toEqual('SUPERMARKET SAVER')
     })
 })
 
